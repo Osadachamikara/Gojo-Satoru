575252ce753f0a4e809847eeeb6012e1e4140cf6
Osadachamikara <107760789+Osadachamikara@users.noreply.github.com>
2022-06-18 20:00:23 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,18 +19,18 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['Your Number']
-global.premium = ['Your Number']
-global.ownernomer = 'Your Number'
-global.ownername = '🐦Star Boy Nexus'
-global.botname = 'Gojo-Satoru'
-global.footer = '©NexusNwInc.'
+global.owner = ['+94752088386']
+global.premium = ['+94752088386']
+global.ownernomer = '+94752088386'
+global.ownername = '🐦OSADA BRO'
+global.botname = 'OSADA BRO'
+global.footer = '©OSADA BRO.'
 global.ig = 'https://github.com/nexusNw'
 global.region = 'India, South India, Kerala'
 global.sc = 'https://github.com/nexusNw/Gojo-Satoru'
-global.myweb = 'https://youtube.com/channel/UCqoUjPvDdb0kjXNYdvPPpHQ'
+global.myweb = 'https://youtube.com/channel/UCrKsUaN2z5Y9ZpISBiVtNjA'
 global.packname = 'Gojo-Satoru'
-global.author = '🐦Star Boy Nexus'
+global.author = '🐦OSADA BRO'
 global.sessionName = 'session'
 global.prefa = ['','!','.','🐦','🐤','🗿']
 global.sp = '⭔'
